a6c0a0d086698c3a0a698820ed4e110d61c6e5c0
Harris <harrisvandenberg@gmail.com>
2021-06-24 11:05:13 +1000

    Added Support & About page
    Added content and CSS basics

--- src/Support.js
+++ src/Support.js
@@ -2,7 +2,8 @@ const Support = () => {
     return (
         <div className="Support">
             <h2>Support</h2>
-            <p>Coming soon</p>
+            <h3>Search results not loading</h3>
+            <p>This can be due to ad blockers or disabling javascript from running through the site.</p>
         </div>
     );
 }
