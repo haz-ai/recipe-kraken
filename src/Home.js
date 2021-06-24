c64edbf7fae4a4d871f43a0ab7477d6d2fe184ff
Harris <harrisvandenberg@gmail.com>
2021-06-24 17:32:36 +1000

    Search Bar formatting
    Added basic formatting to the search bar on the home page

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,50 @@
 import { useState } from "react";
 const Home = () => {
-    const [search, setSearch] = useState('');
+    const [search, setSearch] = useState('Search');
     const [isPending, setIsPending] = useState(false);
     const handleSubmit = (e) => {
         e.preventDefault();
         setIsPending(true);
-        
     }
+
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <label>Search:</label>
-                <input
-                    type="text"
-                    required
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-                {!isPending && <button>Search</button>}
-                {isPending && <button disabled>Searching web....</button>}
-            </form>
-            <p className="Center">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
-            </p>
+            <div className="searching">
+                <form onSubmit={handleSubmit}>
+                    <label for="search">Search for stuff</label>
+                    <input id="search"
+                        type="search"
+                        placeholder="Search..."
+                        autofocus
+                        required
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    {/* <button type="submit">Go</button> */}
+                    {!isPending && <button type="submit">Go</button>}
+                    {isPending && <button disabled type="submit">Searching</button>}
+                </form>
+            </div>
+            <div>
+                <p className="Center">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex natus, quasi debitis omnis aliquam vitae maiores placeat voluptatem amet at quas tempore nostrum vel nihil magni doloremque perferendis laboriosam vero?
+                </p>
+            </div>
         </div>
+
     );
 }
 
